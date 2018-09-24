90d4bb8ea44ba2cc5aa7b46107d24f4361a2166e
Kevin <35283725+kpeter51@users.noreply.github.com>
2018-09-23 18:04:19 -0700

    beginning steps

--- Homework/hw1/Pathfinder.py
+++ Homework/hw1/Pathfinder.py
@@ -11,13 +11,21 @@ from MazeProblem import MazeProblem
 from SearchTreeNode import SearchTreeNode
 import unittest
 
-def solve (problem, initial, goals):
-    # TODO!
-    return []
+
+# Returns estimated cost using Manhattan Distance Heuristic
+def heuristic_cost(state, goal):
+    return abs(goal[1] - state[1]) + abs(goal[0] - state[0])
+
+
+def solve(problem, initial, goals):
+    frontier = set(SearchTreeNode(initial, none,  ))
+    discovered = set()
+
+
 
 
 class PathfinderTests(unittest.TestCase):
-    
+
     def test_maze1(self):
         maze = ["XXXXXXX",
                 "X.....X",
@@ -26,12 +34,12 @@ class PathfinderTests(unittest.TestCase):
                 "XXXXXXX"]
         problem = MazeProblem(maze)
         initial = (1, 3)
-        goals   = [(5, 3)]
+        goals = [(5, 3)]
         soln = solve(problem, initial, goals)
         (soln_cost, is_soln) = problem.soln_test(soln, initial, goals)
         self.assertTrue(is_soln)
         self.assertEqual(soln_cost, 8)
-        
+
     def test_maze2(self):
         maze = ["XXXXXXX",
                 "X.....X",
@@ -40,12 +48,12 @@ class PathfinderTests(unittest.TestCase):
                 "XXXXXXX"]
         problem = MazeProblem(maze)
         initial = (1, 3)
-        goals   = [(3, 3),(5, 3)]
+        goals = [(3, 3), (5, 3)]
         soln = solve(problem, initial, goals)
         (soln_cost, is_soln) = problem.soln_test(soln, initial, goals)
         self.assertTrue(is_soln)
         self.assertEqual(soln_cost, 12)
-        
+
     def test_maze3(self):
         maze = ["XXXXXXX",
                 "X.....X",
@@ -54,12 +62,12 @@ class PathfinderTests(unittest.TestCase):
                 "XXXXXXX"]
         problem = MazeProblem(maze)
         initial = (5, 1)
-        goals   = [(5, 3), (1, 3), (1, 1)]
+        goals = [(5, 3), (1, 3), (1, 1)]
         soln = solve(problem, initial, goals)
         (soln_cost, is_soln) = problem.soln_test(soln, initial, goals)
         self.assertTrue(is_soln)
         self.assertEqual(soln_cost, 12)
-        
+
     def test_maze4(self):
         maze = ["XXXXXXX",
                 "X.....X",
@@ -68,10 +76,10 @@ class PathfinderTests(unittest.TestCase):
                 "XXXXXXX"]
         problem = MazeProblem(maze)
         initial = (5, 1)
-        goals   = [(5, 3), (1, 3), (1, 1)]
+        goals = [(5, 3), (1, 3), (1, 1)]
         soln = solve(problem, initial, goals)
         self.assertTrue(soln == None)
-    
+
 
 if __name__ == '__main__':
     unittest.main()
