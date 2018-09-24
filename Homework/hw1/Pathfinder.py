dbd7d4726e0c8c73cf4ef2706e42d6609f21afdb
Kevin <35283725+kpeter51@users.noreply.github.com>
2018-09-23 20:26:17 -0700

    further progress on solve

--- Homework/hw1/Pathfinder.py
+++ Homework/hw1/Pathfinder.py
@@ -13,6 +13,21 @@ import unittest
 from queue import PriorityQueue
 
 
+def generate_path(node):
+    solution = []
+    solution.append(node.action)
+
+    parentNode = node.parent
+
+    while (parentNode.action != None):
+        solution.append(parentNode.action)
+        parentNode = parentNode.parent
+
+    solution.reverse()
+
+    return solution
+
+
 def h(state, goal):
     return abs(goal[1] - state[1]) + abs(goal[0] - state[0])
 
@@ -23,73 +38,77 @@ def solve(problem, initial, goals):
 
     # Put initial state in queue
     frontier.put_nowait(
-        (0, SearchTreeNode(initial, None, None, 0, h(initial, goals[1]))))
+        (0, SearchTreeNode(initial, None, None, 0, h(initial, goals[0]))))
 
     while not frontier.empty():
         current = frontier.get_nowait()[1]
-        for neighbor in MazeProblem.transitions(current.state):
-            new_node = SearchTreeNode(neighbor[2], neighbor[0], current, current.totalCost + neighbor[1], h(neighbor[2])
 
-            if neighbor[2] in graveyard:
+        if current.state == goals[0]:
+            return generate_path(current)
+
+        for neighbor in problem.transitions(current.state):
+            new_node = SearchTreeNode(neighbor[2], neighbor[0], current, current.totalCost + neighbor[1],
+                                      h(neighbor[2], goals[0]))
+
+            if neighbor[2] not in graveyard:
                 frontier.put_nowait(
                     ((new_node.totalCost + new_node.heuristicCost), new_node))
-
 
 
 class PathfinderTests(unittest.TestCase):
 
     def test_maze1(self):
-        maze=["XXXXXXX",
+        maze = ["XXXXXXX",
                 "X.....X",
                 "X.M.M.X",
                 "X.X.X.X",
                 "XXXXXXX"]
-        problem=MazeProblem(maze)
-        initial=(1, 3)
-        goals=[(5, 3)]
-        soln=solve(problem, initial, goals)
-        (soln_cost, is_soln)=problem.soln_test(soln, initial, goals)
+        problem = MazeProblem(maze)
+        initial = (1, 3)
+        goals = [(5, 3)]
+        soln = solve(problem, initial, goals)
+        (soln_cost, is_soln) = problem.soln_test(soln, initial, goals)
         self.assertTrue(is_soln)
         self.assertEqual(soln_cost, 8)
 
     def test_maze2(self):
-        maze=["XXXXXXX",
+        maze = ["XXXXXXX",
                 "X.....X",
                 "X.M.M.X",
                 "X.X.X.X",
                 "XXXXXXX"]
-        problem=MazeProblem(maze)
-        initial=(1, 3)
-        goals=[(3, 3), (5, 3)]
-        soln=solve(problem, initial, goals)
-        (soln_cost, is_soln)=problem.soln_test(soln, initial, goals)
+        problem = MazeProblem(maze)
+        initial = (1, 3)
+        goals = [(3, 3), (5, 3)]
+        soln = solve(problem, initial, goals)
+        (soln_cost, is_soln) = problem.soln_test(soln, initial, goals)
         self.assertTrue(is_soln)
         self.assertEqual(soln_cost, 12)
 
     def test_maze3(self):
-        maze=["XXXXXXX",
+        maze = ["XXXXXXX",
                 "X.....X",
                 "X.M.MMX",
                 "X...M.X",
                 "XXXXXXX"]
-        problem=MazeProblem(maze)
-        initial=(5, 1)
-        goals=[(5, 3), (1, 3), (1, 1)]
-        soln=solve(problem, initial, goals)
-        (soln_cost, is_soln)=problem.soln_test(soln, initial, goals)
+        problem = MazeProblem(maze)
+        initial = (5, 1)
+        goals = [(5, 3), (1, 3), (1, 1)]
+        soln = solve(problem, initial, goals)
+        (soln_cost, is_soln) = problem.soln_test(soln, initial, goals)
         self.assertTrue(is_soln)
         self.assertEqual(soln_cost, 12)
 
     def test_maze4(self):
-        maze=["XXXXXXX",
+        maze = ["XXXXXXX",
                 "X.....X",
                 "X.M.XXX",
                 "X...X.X",
                 "XXXXXXX"]
-        problem=MazeProblem(maze)
-        initial=(5, 1)
-        goals=[(5, 3), (1, 3), (1, 1)]
-        soln=solve(problem, initial, goals)
+        problem = MazeProblem(maze)
+        initial = (5, 1)
+        goals = [(5, 3), (1, 3), (1, 1)]
+        soln = solve(problem, initial, goals)
         self.assertTrue(soln == None)
 
 
