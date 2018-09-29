75f0ed7b4281d184ef0f5fe0b003ee3ed5260e70
Kevin <35283725+kpeter51@users.noreply.github.com>
2018-09-28 19:25:47 -0700

    Added many comments

--- Homework/hw1/Pathfinder.py
+++ Homework/hw1/Pathfinder.py
@@ -15,9 +15,12 @@ import numpy
 from itertools import permutations
 
 
-# Same code from Classwork #1
-# Follows a node's parent tree to generate the path it took
 def generate_path(node):
+    """Follows a node's parent tree to generate the path it took
+
+    :param node: node whose parents should be looked up
+    :return:Path of actions taken to reach notes state ("U","D","L","R")
+    """
     solution = [node.action]
 
     parent_node = node.parent
@@ -32,10 +35,23 @@ def generate_path(node):
 
 
 def h(state, goal):
+    """Manhattan Distance Heuristic between 2 points
+
+    :param state: starting state
+    :param goal: ending state
+    :return: score from heuristic
+    """
     return abs(goal[1] - state[1]) + abs(goal[0] - state[0])
 
 
 def a_star(problem, initial, goal):
+    """Uses an A* search to find the optimal path between 2 points
+
+    :param problem: maze problem
+    :param initial: starting state
+    :param goal: final state
+    :return: (totalCost, path)
+    """
     frontier = PriorityQueue(maxsize=0)
     graveyard = set()
 
@@ -45,8 +61,7 @@ def a_star(problem, initial, goal):
 
     while not frontier.empty():
 
-        # node with lowest h(n) + g(n) score
-        current = frontier.get_nowait()
+        current = frontier.get_nowait()  # node with lowest h(n) + g(n) score
         graveyard.add(current)
 
         # If it satisfies the goal, return its path/solution
@@ -65,6 +80,11 @@ def a_star(problem, initial, goal):
 
 
 def reverse_path(path):
+    """Gives the reverse of an input put. All instructions flipped and order flipped
+
+    :param path: path to reverse
+    :return: reversed path
+    """
     reverse_dictionary = {"D": "U", "U": "D", "L": "R", "R": "L"}
     new_path = []
 
@@ -77,27 +97,39 @@ def reverse_path(path):
 
 
 def solve(problem, initial, goals):
+    """Uses A* to create a cost matrix that is used to evaluate a TSP like problem
+
+    :param problem: maze
+    :param initial: starting state
+    :param goals: all goals to reach
+    :return: optimal path
+    """
     key_states = [initial] + goals
     length = len(key_states)
 
-    cost_matrix = numpy.zeros((length, length), object)
+    cost_matrix = numpy.zeros((length, length), object)  # Stores A* cost and path for any 2 points
 
+    # Fill in the cost matrix with A* results
     for y in range(length - 1):
         for x in range(y + 1, length):
-            if x != y:
-                cost_matrix[x][y] = a_star(problem, key_states[x], key_states[y])
-                if cost_matrix[x][y] is None:
+            if x != y:  # Ignore state traveling to itself
+                cost_matrix[x][y] = a_star(problem, key_states[x], key_states[y])  # A* result between 2 points
+                if cost_matrix[x][y] is None:  # If a state can't be reached, return None for the path
                     return None
+                # Infer the bottom half of the matrix by reversing the path of the top half
                 cost_matrix[y][x] = (cost_matrix[x][y][0], reverse_path(cost_matrix[x][y][1]))
 
+    # All possible orders of goals, always starting at initial
     all_permutations = [perm for perm in permutations(key_states) if perm[0] == key_states[0]]
 
-    index_dictionary = {}
+    index_dictionary = {}  # Dictionary so that the cost_matrix can be called upon using a goal
     for state in key_states:
         index_dictionary[state] = key_states.index(state)
 
-    all_costs = []
-    counter = 0
+    all_costs = []  # A list meant to parallel the all_permutations list, holding each perms respective cost
+    counter = 0  # To keep track of index for all_costs
+
+    # Generate the cost for each permutation and store it in all_costs
     for perm in all_permutations:
         total_cost = 0
         for s in range(length - 1):
@@ -105,10 +137,10 @@ def solve(problem, initial, goals):
         all_costs.append(total_cost)
         counter += 1
 
-    optimal_perm = all_permutations[all_costs.index(min(all_costs))]
-    print(optimal_perm)
+    optimal_perm = all_permutations[all_costs.index(min(all_costs))]  # A permutation with the lowest cost
     final_path = []
 
+    # Compile the path for this permutation using the cost_matrix
     for s in range(length - 1):
         final_path += cost_matrix[index_dictionary[optimal_perm[s]]][index_dictionary[optimal_perm[s + 1]]][1]
 
